bbb7fb643a0203de2b3627564dca6283310f1731
Voron <67822791+VoronTheChell@users.noreply.github.com>
2024-09-11 16:17:57 +0400

    Version 8.0
    Fix bugs in menu select outputs Pokemons.
    Add new parameters Action Attacks in class Pokemon.
    Output write Actions Attacks Pokemons in statistics.
    Add values Actrions Attacks in: Litwick, Lampent, and Chandelure.

--- Program.cs
+++ Program.cs
@@ -17,18 +17,37 @@ namespace ParktickWork___Pokemon_Class
         public int special_defense { get; set; } // Спец. Защита Няшки
         public int Speed { get; set; } // Скорость няшки
 
+        public string ActionAttack1 { get; set; } // Действие Няшки 1
+        public string ActionAttack2 { get; set; } // Действие Няшки 2
+        public string ActionAttack3 { get; set; } // Действие Няшки 3
+        public string ActionAttack4 { get; set; } // Действие Няшки 4
+
+
+
         // Здесь мы делаем публичныйм переменную Pokemon для записи няшек
-        public Pokemon(string name, string type1, int hp, int attack, int defense, int specialAttack, int specialDefense, int speed, string type2 = null) 
+        public Pokemon(string name, string type1, int hp, int attack, int defense, 
+                        int specialAttack, int specialDefense, int speed, string type2 = null,
+                        string actionAttack1 = null, string actionAttack2 = null, string actionAttack3 = null, 
+                        string actionAttack4 = null) 
         {
             Name = name;
+
             Type1 = type1;
             Type2 = type2;
+
             HP = hp;
             Attack = attack;
             Defense = defense;
+
             special_attack = specialAttack;
             special_defense = specialDefense;
+
             Speed = speed;
+
+            ActionAttack1 = actionAttack1;
+            ActionAttack2 = actionAttack2;
+            ActionAttack3 = actionAttack3;
+            ActionAttack4 = actionAttack4;
         }
 
         // Вывод статистики няшек
@@ -37,11 +56,23 @@ namespace ParktickWork___Pokemon_Class
             if (Type2 != null) // Если тип 2 заполнен выводить его
             {
                 Console.WriteLine($"Name: Litwick \nType: {Type1} & {Type2} \nHP:{HP} PT \nAttack:{Attack} PT \nDefense:{Defense} PT \nSpecial Attack: {special_attack} PT \nSpecial Defense: {special_defense} PT \nSpeed:{Speed} PT");
+                Console.WriteLine($"Action Attacks:");
+                Console.WriteLine(ActionAttack1);
+                Console.WriteLine(ActionAttack2);
+                Console.WriteLine(ActionAttack3);
+                Console.WriteLine(ActionAttack4);
+                Console.WriteLine();
             }
 
             else // В инном случаи не выводить
             {
                 Console.WriteLine($"Name: Litwick \nType: {Type1} \nHP:{HP} PT \nAttack:{Attack} PT \nDefense:{Defense} PT \nSpecial Attack: {special_attack} PT \nSpecial Defense: {special_defense} PT \nSpeed:{Speed} PT");
+                Console.WriteLine($"Action Attacks:");
+                Console.WriteLine(ActionAttack1);
+                Console.WriteLine(ActionAttack2);
+                Console.WriteLine(ActionAttack3);
+                Console.WriteLine(ActionAttack4);
+                Console.WriteLine();
             }
         }
     }
@@ -49,17 +80,17 @@ namespace ParktickWork___Pokemon_Class
     // Записываем Няшки
     public class Litwick : Pokemon
     {
-        public Litwick() : base("Litwick", "Ghost", 50, 30, 55, 65, 55, 20, "Fire") { }
+        public Litwick() : base("Litwick", "Ghost", 50, 30, 55, 65, 55, 20, "Fire", "Rest", "Double Team") { }
     }
 
     public class Lampent : Pokemon
     {
-        public Lampent() : base("Lampent", "Ghost", 60, 40, 60, 95, 60, 55, "Fire") { }
+        public Lampent() : base("Lampent", "Ghost", 60, 40, 60, 95, 60, 55, "Fire", "Rest", "Double Team", "Ominous Wind") { }
     }
 
     class Chandelure : Pokemon
     {
-        public Chandelure() : base("Chandelure", "Ghost", 60, 55, 90, 145, 90, 80, "Fire") { }
+        public Chandelure() : base("Chandelure", "Ghost", 60, 55, 90, 145, 90, 80, "Fire", "Rest", "Double Team", "Ominous Wind", "Refresh") { }
     }
 
     internal class Program
@@ -70,51 +101,62 @@ namespace ParktickWork___Pokemon_Class
 
             while (workApp)
             {
-                Console.WriteLine("Select Pokemon:\n1) Litwick\n2) Lampent\n3) Chandelure");
-                Console.Write("Write number Pokemon: ");
-                int select = int.Parse(Console.ReadLine());
-
-                switch (select)
+                try
                 {
-                    case 1:
-                        {
-                            Console.Clear();
-                            Litwick litwick = new Litwick();
-                            litwick.statics();
-                            Console.WriteLine("Press any key...");
-                            Console.ReadLine();
-                            Console.Clear();
-                            break;
-                        }
+                    Console.WriteLine("Select Pokemon:\n1) Litwick\n2) Lampent\n3) Chandelure");
+                    Console.Write("Write number Pokemon: ");
+                    int select = int.Parse(Console.ReadLine());
 
-                    case 2:
-                        {
-                            Console.Clear();
-                            Lampent lampent = new Lampent();
-                            lampent.statics();
-                            Console.WriteLine("Press any key...");
-                            Console.ReadLine();
-                            Console.Clear();
-                            break;
-                        }
+                    switch (select)
+                    {
+                        case 1:
+                            {
+                                Console.Clear();
+                                Litwick litwick = new Litwick();
+                                litwick.statics();
+                                Console.WriteLine("Press any key...");
+                                Console.ReadLine();
+                                Console.Clear();
+                                break;
+                            }
 
-                    case 3:
-                        {
-                            Console.Clear();
-                            Chandelure chandelure = new Chandelure();
-                            chandelure.statics();
-                            Console.WriteLine("Press any key...");
-                            Console.ReadLine();
-                            Console.Clear();
-                            break;
-                        }
+                        case 2:
+                            {
+                                Console.Clear();
+                                Lampent lampent = new Lampent();
+                                lampent.statics();
+                                Console.WriteLine("Press any key...");
+                                Console.ReadLine();
+                                Console.Clear();
+                                break;
+                            }
 
-                    case 0:
-                        {
-                            Console.Clear();
-                            workApp = false;
-                            break;
-                        }
+                        case 3:
+                            {
+                                Console.Clear();
+                                Chandelure chandelure = new Chandelure();
+                                chandelure.statics();
+                                Console.WriteLine("Press any key...");
+                                Console.ReadLine();
+                                Console.Clear();
+                                break;
+                            }
+
+                        case 0:
+                            {
+                                Console.Clear();
+                                workApp = false;
+                                break;
+                            }
+                    }
+                }
+
+                catch
+                {
+                    Console.WriteLine("Error: You enter simwole not number!\nPlease try again!");
+                    Console.WriteLine("Press any key...");
+                    Console.ReadLine();
+                    Console.Clear();
                 }
             }
         }
