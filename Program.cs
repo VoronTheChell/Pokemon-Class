040e8e5c30ff0a2a302e1d889f7fb23b48ce09ee
Voron <67822791+VoronTheChell@users.noreply.github.com>
2024-09-11 13:14:31 +0400

    Version 1.0
    Add base class: Pokemon

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ParktickWork___Pokemon_Class
+{
+    // Создаем общий класс Pokemon для хранения няшек
+    public class Pokemon
+    {
+        public string Name { get; set; } // Имя няшки
+        public string Type1 { get; set; } // Тип для няшки
+        public string Type2 { get; set; } // может быть null, если тип один для няшки
+
+        public int HP { get; set; } // Здоровье няшки
+        public int Attack { get; set; } // Аттака няшки
+        public int Defense { get; set; } // Защита няшки
+
+        public int special_attack { get; set; } // Спец. Аттака Няшки
+        public int special_defense { get; set; } // Спец. Защита Няшки
+        public int Speed { get; set; } // Скорость няшки
+
+        // Здесь мы делаем публичныйм переменную Pokemon для записи няшек
+        public Pokemon(string name, string type1, int hp, int attack, int defense, int specialAttack, int specialDefense, int speed, string type2 = null) 
+        {
+            Name = name;
+            Type1 = type1;
+            Type2 = type2;
+            HP = hp;
+            Attack = attack;
+            Defense = defense;
+            special_attack = specialAttack;
+            special_defense = specialDefense;
+            Speed = speed;
+        }
+
+        // Вывод статистики няшек
+        public void statics()
+        {
+            if (Type2 != null) // Если тип 2 заполнен выводить его
+            {
+                Console.WriteLine($"Name: Litwick \nType: {Type1} & {Type2} \nHP:{HP} PT \nAttack:{Attack} PT \nDefense:{Defense} PT \nSpecial Attack: {special_attack} PT \nSpecial Defense: {special_defense} PT \nSpeed:{Speed} PT");
+            }
+
+            else // В инном случаи не выводить
+            {
+                Console.WriteLine($"Name: Litwick \nType: {Type1} \nHP:{HP} PT \nAttack:{Attack} PT \nDefense:{Defense} PT \nSpecial Attack: {special_attack} PT \nSpecial Defense: {special_defense} PT \nSpeed:{Speed} PT");
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
+
+// Брал инфу о покемонах (ну или няшек как здесь :) ) с этой сайта: https://pokemondb.net
